81d1bad180a7915ac1ef94fd029ec747cd3b4d0f
Isay Konter <kwekkwekpatu@Isays-Laptop.local>
2024-08-20 18:27:57 +0200

    Added a moveable object for the player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player as pl
 
 def main():
     print("Starting asteroids!")
@@ -7,13 +8,35 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = pl.Player(x= SCREEN_WIDTH / 2, y= SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    pl.Player.containers = (updatable, drawable)
+
+    updatable.add(player)
+    drawable.add(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill(color="black")
+        for element in updatable:
+            element.update(dt)
+
+        screen.fill("black")
+
+        for element in drawable:
+            element.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
