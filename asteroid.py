3fc62cc6ab661ac80acf38b786170c951d95ab1f
Isay Konter <kwekkwekpatu@Isays-Laptop.local>
2024-08-20 19:47:01 +0200

    Added asteroid splitting from bullets

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from circleshape import CircleShape
 from constants import *
@@ -11,3 +12,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        positive_vector = self.velocity.rotate(random_angle)
+        negative_vector = self.velocity.rotate(-random_angle)
+        radius = self.radius - ASTEROID_MIN_RADIUS
+        x, y = self.position
+        positive_asteroid = Asteroid(x, y, radius)
+        negative_asteroid = Asteroid(x, y, radius)
+
+        positive_asteroid.velocity = positive_vector * 1.2
+        negative_asteroid.velocity = negative_vector * 1.2
